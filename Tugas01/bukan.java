96e277138f5c5887bdfe75f07d0aec34cef07655
Rizqimid29 <ruzqifahrudieen@student.ub.ac.id>
2024-03-07 13:46:04 +0700

    Initial Commit

--- Tugas01/bukan.java
+++
@@ -1,7 +0,0 @@
-package Tugas01;
-
-public class bukan {
-    public static void main(String[] args) {
-        System.out.println("udin");
-    }
-}
--- Tugas02/halo.java
+++
@@ -1,7 +0,0 @@
-package Tugas02;
-
-public class halo {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
